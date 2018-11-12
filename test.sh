5cfb517005450d93eb596050cbd58617cf650813
Tomáš Chaloupka <chalucha@gmail.com>
2018-11-12 18:03:22 +0100

    change

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
+echo "root=${ROOT_FOLDER}"
 cd testrepo
 echo -n "version="`git describe | tr -d '\n'` > files/build.properties
 ls -la /
 ls -la /build
 ls -la /files
+ls -la ${ROOT_FOLDER}
