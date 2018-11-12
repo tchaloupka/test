e1c4deecac7eb59b16196b7062a1f2a83f1a418e
Tomáš Chaloupka <chalucha@gmail.com>
2018-11-12 18:08:43 +0100

    ch

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-echo "root=${ROOT_FOLDER}"
 echo "pwd=$PWD"
 cd testrepo
-echo -n "version="`git describe | tr -d '\n'` > files/build.properties
-ls -la /
-ls -la /build
-ls -la /files
-ls -la ${ROOT_FOLDER}
+echo -n "version="`git describe | tr -d '\n'` > ../files/build.properties
+ls -la $PWD
+ls -la $PWD/files
+ls -la $PWD/testrepo
+
